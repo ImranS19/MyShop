a10bb911aeb6a4c0f348dcacf13960ab4642bb40
Imran S <55154759+ImranS19@users.noreply.github.com>
2023-04-02 12:23:20 -0400

    Added components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+   <footer>
+    <Container>
+      <Row>
+        <Col classname ="text-center py3">Copyright &copy; MyShop</Col>
+      </Row>
+    </Container>
+   </footer>
+  )
+}
+
+export default Footer 
